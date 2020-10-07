11623aa440355513584892beca4f16247f415347
EK <ekim0480@gmail.com>
2020-10-07 16:45:37 -0500

    first commit, node modules, gitignore, schema

---
+++ schema.sql
@@ -0,0 +1,30 @@
+DROP DATABASE IF EXISTS employee_db;
+
+CREATE DATABASE employee_db;
+ 
+ USE employee_db;
+
+ CREATE TABLE department (
+    id INTEGER(10) AUTO_INCREMENT NOT NULL,
+    name VARCHAR(30),
+    PRIMARY KEY(id)
+ );
+
+ CREATE TABLE role (
+ 	id INTEGER(10) AUTO_INCREMENT NOT NULL,
+    title VARCHAR(30),
+    salary DECIMAL(10,2),
+    department_id INTEGER(10),
+    PRIMARY KEY(id),
+    FOREIGN KEY(department_id) REFERENCES department(id)
+ );
+ 
+ CREATE TABLE employee (
+    id INTEGER(10) AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INTEGER(5),
+    manager_id INTEGER(5),
+    PRIMARY KEY(id),
+    FOREIGN KEY(role_id) REFERENCES role(id)
+ );
